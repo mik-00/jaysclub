e8b37c24f49d6424b08498ac913787493e4e3a13
Mikayla Pichonsky <mikayla.t.pichonsky@gmail.com>
2023-08-10 13:16:05 -0600

    Pre-deployment

--- jaysclub/settings.py
+++ jaysclub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,13 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+# Added while completing final production checklist
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_SECONDS = 60
 SESSION_COOKIE_SECURE = True
